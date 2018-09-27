6e61220d6bdbc8582c6692a09a81a5c650e82b87
StingerWin <witalikwin@gmail.com>
2018-09-27 23:06:49 +0300

    робота з контактною фотмою, повідомлення помили і відправлення

--- controllers/index.php
+++ controllers/index.php
@@ -38,6 +38,7 @@ Class Controller_Index Extends Controller_Base
 
 
       $errors = array();
+      $send = '';
       if (isset($data['send'])) {
         $name_client = trim($data['name']);
         $email_client = $data['email'];
@@ -47,24 +48,40 @@ Class Controller_Index Extends Controller_Base
         $stan_massage = $data['stan_massage'];
 
         if (empty($name_client)) {
-          $errors = 'Заповніть поле імя!';
-        }
-        elseif (empty($email_client)) {
-          $errors = 'Заповніть поле email!';
-        }
-        elseif (empty($coment_client)) {
-          $errors = 'Заповніть поле текст!';
-        }
+              if ($_SESSION['lang'] == 'ru') {$errors = 'Вы не заполнили поле "ваше имя"!';
+              }
+              if ($_SESSION['lang'] == 'en') {$errors = 'You have not filled out the field "your name"!';}
+              if ($_SESSION['lang'] == 'ua') {$errors = 'Ви не заповнили поле "ваше ім`я"!';}
+            }
+            elseif (empty($email_client)) {
+              if ($_SESSION['lang'] == 'ru') {$errors = 'Вы не заполнили поле "ваш email"!';
+              }
+              if ($_SESSION['lang'] == 'en') {$errors = 'You have not filled out the field "your email"!';}
+              if ($_SESSION['lang'] == 'ua') {$errors = 'Ви не заповнили поле "ваш email"!';}
+            }
+            elseif (empty($coment_client)) {
+              if ($_SESSION['lang'] == 'ru') {$errors = 'Вы не заполнили поле "ваш коментар"!';
+              }
+              if ($_SESSION['lang'] == 'en') {$errors = 'You have not filled out the field "your comment"!';}
+              if ($_SESSION['lang'] == 'ua') {$errors = 'Ви не заповнили поле "ваш коментар"!';}
+            }
 
         if (empty($errors)) {
           $query = "INSERT INTO `message_client`(`id`, `name_client`, `email_client`, `coment_client`, `date`, `time`, `stan_massage`) VALUES (null,'$name_client','$email_client','$coment_client','$date','$time','$stan_massage')";
           $result = mysqli_query($db, $query);
 
+          if (isset($result)) {
+            if ($_SESSION['lang'] == 'ru') {$send = 'Ваше сообщение успешно отправлено!';
+              }
+              if ($_SESSION['lang'] == 'en') {$send = 'Your message has been successfully sent!';}
+              if ($_SESSION['lang'] == 'ua') {$send = 'Ваше повідомлення успішно відправлено!';}
+          }
         }
 
     }
 
     $template->set('errors', $errors);
+    $template->set('send', $send);
 
     $this->_renderLayout($template);
 }
